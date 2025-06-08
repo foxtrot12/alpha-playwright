9d89492e7ef71b94d16dfa295a98fc4807fe8d2e
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2025-06-08 07:49:50 +0000

    1st push

---
+++ src/app/components/imageGallery/imageGallery.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "image-gallery",
+    templateUrl:"./imageGallery.component.html",
+    standalone: true,
+    styleUrl:"./imageGallery.styles.scss"
+})
+
+export class ImageGallery{
+
+    #pixabayKey = '50706113-a9b1509434f56c52299d7dfdb'
+
+    
+}
